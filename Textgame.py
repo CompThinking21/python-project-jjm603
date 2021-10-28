95d1f520741d82908a1e1cfe85f3c46b7a132159
jjm603 <jjm603@scarletmail.rutgers.edu>
2021-10-28 16:10:21 -0400

    Update Textgame.py
    added comments

--- Textgame.py
+++ Textgame.py
@@ -1,9 +1,10 @@
 import random
-
+#This a rock paper scissors game. It takes a play(Rock, paper or scissors) from the user and put it against the computer. The computer randomly picks a play and goes against the user. I use if statements to compare the plays against each other and print out a string that says who wins.
 user = input('Rock, Paper or Scissors:'+ ' ')
+#this takes a play from the user and puts it against a randomized computer play
 plays = ['rock' , 'paper' , 'scissors']
 computer = random.choice(plays)
-
+#This is to have the computer opponent pick randomly from the plays list.
 if user == computer:
     print("Both have selected the same play")
 elif user == 'rock':
@@ -18,6 +19,7 @@ elif user == 'paper':
         print('Scissors cuts Paper! You Lose...')
 elif user == 'scissors':
     if computer == 'paper':
-        print('scissors cuts paper! You Win!!!')
+        print('Scissors cuts paper! You Win!!!')
     else:
         print('Rock smashed Scissors! You Lose...')
+#these are all the results for the outcomes of a rock paper scissors game. It then prints to the terminal what the player and computer chose and who won.
