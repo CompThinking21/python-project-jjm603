ef69423fc4de635124c2f56dd81d2fa1dd331a30
jjm603 <jjm603@scarletmail.rutgers.edu>
2021-12-02 19:03:56 -0500

    Project
    comments and fixes. Added hangman game.

---
+++ README.txt
@@ -0,0 +1,24 @@
+I wanted to make a dungeon, where the user would be able to run through the dungeon and play games at each floor to eventually make it to the end.
+You would have to win a game to move onto the next one. The computer the player is playing against would be a master of the game and it would be challenging to defeat them.
+Once you defeat all of the masters, you make it to the end and you win. I didn't end up making the full story, but I did end up making the games.
+I created a main menu that the user can use to play each game. Once each game is done, it takes you back to the main menu and you can then choose to play another game.
+
+Creating the project was a bit difficult. Creating games just using python is hard because it has to be text based. I thought about making a game that shows up in another window, but
+I didn't know how to fully implement it or use it in the code. I eventually found out I could make rock paper scissors and that's where I started.
+Reaching the first milestone wasn't that hard since all we had to do was make a function. I made the rock paper scissors game and it required a lot of if statements.
+It wasn't hard to make, but it required a lot of repetition. For the second milestone I needed to create something that had an output. I already had this with the rock paper scissors
+game, but there was no way to run other games along side it. There was also no way for the player to pick the game they were playing. I then had to make a main menu so
+the user could move through games easily. The main menu was difficult make, because I didn't know where to start. My professor helped me get it started, and then it
+just worked for every other game that I implemented. I also added a higher lower game, where the user has to guess if the number chosen is above, below or equal to 50.
+This game was very similar to the rock paper scissors game and was easy to add in. For the final part of the project I added a hangman game. This game was very difficult to make.
+I didn't know how to make the game read each letter and fill the word in. I also didn't know how to eliminate letters after they were already guessed. I had to look
+over different peoples projects and make sense of what they were doing to create my own. It turned out well though and does exactly what I wanted it to do. If I were to do the project
+again, I would have focused more on trying to do my original idea. I didn't really focus on making the story of the game or making it a linear experience, where the player has to
+complete one task in order to get to the next one. I focused and put a lot of time in trying to get the games to work right and I forgot about trying to make a story along with it.
+
+Some corrections I got was to make the program more robust. My first milestone was just a game and it wasn't even inside of a function. I had to put it into a function and then
+make it able to be called from the main menu. Once I put it in the function I made the main menu call the function when the user typed in the number 1. I also put the main menu
+function after each if statement so that after the game was over it would go back to the main menu. I was also told to make my code stronger, because it didn't filter out any wrong
+words. If someone put in the incorrect word then the program just wouldn't do anything. I then added to my if statements an option that would ask the user to input again if the word
+didn't work. The games would also end after each play through and I needed a way to allow the user to continuously play games. I added the main menu function into the if statements as well
+and it put the player back into the main menu to pick another game.
--- Textgame.py
+++ Textgame.py
@@ -1,17 +1,21 @@
 import random
+#This function creates a mainmenu, where the user can choose which game they would like to play. Once every game is finished, the player is returned back to the main menu and can pick again
 def mainmenu():
-    user = str(input('Welcome to the Main Menu. There are many rooms with games and their respective masters. There are 4 rooms, choose a room to challenge an opponent or input 5 to quit:'+' ')).lower()
+    user = str(input('Welcome to the Main Menu. There are many rooms with games and their respective masters. There are 3 rooms, choose a room to challenge an opponent or input 5 to quit:'+' ')).lower()
     if user == '1':
         rps()
     elif user == '2':
         highlow()
+    elif user == '3':
+        hang()
     elif user == '5':
         quit()
-
+#This is a game called higher or lower. The player is prompted to guess if the random number chosen is above, below or equal to 50.
 def highlow():
     user = str(input('Is the number higher, lower or equal to 50:'+ ' ')).lower()
+#this line of code takes the user input and puts it in lowercase characters.
     computer = random.randint(1,100)
-
+#I use if statements to tell whether or not the player was correct and send them back to the main menu afterwards.
     if user == 'higher' and (computer > 50):
         print('The Number is Higher. You Win!!!')
         mainmenu()
@@ -76,12 +80,287 @@ def rps():
     else:
         print('No option selected, try again.')
         rps()
-        # raise TypeError("That is not an option.")
-#these are all the results for the outcomes of a rock paper scissors game. It then prints to the terminal what the player and computer chose and who won.
-mainmenu()
 
+#these are all the results for the outcomes of a rock paper scissors game. It then prints to the terminal what the player and computer chose and who won.
+
+#This is a hangman game. The player has to guess the letters or guess the word in order to win the game.
 def hang():
-    words = ['dog', 'cat', 'monster']
-    word = random.choice(words)
-    user = str(input('Guess a letter:'+ ' ')).lower()
-    
+#a list of animals that are going to be the words that the player has to guess.
+    words = [ "Aardvark",
+    "Albatross",
+    "Alligator",
+    "Alpaca",
+    "Ant",
+    "Anteater",
+    "Antelope",
+    "Ape",
+    "Armadillo",
+    "Donkey",
+    "Baboon",
+    "Badger",
+    "Barracuda",
+    "Bat",
+    "Bear",
+    "Beaver",
+    "Bee",
+    "Bison",
+    "Boar",
+    "Buffalo",
+    "Butterfly",
+    "Camel",
+    "Capybara",
+    "Caribou",
+    "Cassowary",
+    "Cat",
+    "Caterpillar",
+    "Cattle",
+    "Chamois",
+    "Cheetah",
+    "Chicken",
+    "Chimpanzee",
+    "Chinchilla",
+    "Chough",
+    "Clam",
+    "Cobra",
+    "Cockroach",
+    "Cod",
+    "Cormorant",
+    "Coyote",
+    "Crab",
+    "Crane",
+    "Crocodile",
+    "Crow",
+    "Curlew",
+    "Deer",
+    "Dinosaur",
+    "Dog",
+    "Dogfish",
+    "Dolphin",
+    "Dotterel",
+    "Dove",
+    "Dragonfly",
+    "Duck",
+    "Dugong",
+    "Dunlin",
+    "Eagle",
+    "Echidna",
+    "Eel",
+    "Eland",
+    "Elephant",
+    "Elk",
+    "Emu",
+    "Falcon",
+    "Ferret",
+    "Finch",
+    "Fish",
+    "Flamingo",
+    "Fly",
+    "Fox",
+    "Frog",
+    "Gaur",
+    "Gazelle",
+    "Gerbil",
+    "Giraffe",
+    "Gnat",
+    "Gnu",
+    "Goat",
+    "Goldfinch",
+    "Goldfish",
+    "Goose",
+    "Gorilla",
+    "Goshawk",
+    "Grasshopper",
+    "Grouse",
+    "Guanaco",
+    "Gull",
+    "Hamster",
+    "Hare",
+    "Hawk",
+    "Hedgehog",
+    "Heron",
+    "Herring",
+    "Hippopotamus",
+    "Hornet",
+    "Horse",
+    "Human",
+    "Hummingbird",
+    "Hyena",
+    "Ibex",
+    "Ibis",
+    "Jackal",
+    "Jaguar",
+    "Jay",
+    "Jellyfish",
+    "Kangaroo",
+    "Kingfisher",
+    "Koala",
+    "Kookabura",
+    "Kouprey",
+    "Kudu",
+    "Lapwing",
+    "Lark",
+    "Lemur",
+    "Leopard",
+    "Lion",
+    "Llama",
+    "Lobster",
+    "Locust",
+    "Loris",
+    "Louse",
+    "Lyrebird",
+    "Magpie",
+    "Mallard",
+    "Manatee",
+    "Mandrill",
+    "Mantis",
+    "Marten",
+    "Meerkat",
+    "Mink",
+    "Mole",
+    "Mongoose",
+    "Monkey",
+    "Moose",
+    "Mosquito",
+    "Mouse",
+    "Mule",
+    "Narwhal",
+    "Newt",
+    "Nightingale",
+    "Octopus",
+    "Okapi",
+    "Opossum",
+    "Oryx",
+    "Ostrich",
+    "Otter",
+    "Owl",
+    "Oyster",
+    "Panda"
+    "Panther",
+    "Parrot",
+    "Partridge",
+    "Peafowl",
+    "Pelican",
+    "Penguin",
+    "Pheasant",
+    "Pig",
+    "Pigeon",
+    "Pony",
+    "Porcupine",
+    "Porpoise",
+    "Quail",
+    "Quelea",
+    "Quetzal",
+    "Rabbit",
+    "Raccoon",
+    "Rail",
+    "Ram",
+    "Rat",
+    "Raven",
+    "Reindeer",
+    "Rhinoceros",
+    "Rook",
+    "Salamander",
+    "Salmon",
+    "Sandpiper",
+    "Sardine",
+    "Scorpion",
+    "Seahorse",
+    "Seal",
+    "Shark",
+    "Sheep",
+    "Shrew",
+    "Skunk",
+    "Snail",
+    "Snake",
+    "Sparrow",
+    "Spider",
+    "Spoonbill",
+    "Squid",
+    "Squirrel",
+    "Starling",
+    "Stingray",
+    "Stinkbug",
+    "Stork",
+    "Swallow",
+    "Swan",
+    "Tapir",
+    "Tarsier",
+    "Termite",
+    "Tiger",
+    "Toad",
+    "Trout",
+    "Turkey",
+    "Turtle",
+    "Viper",
+    "Vulture",
+    "Wallaby",
+    "walrus",
+    "wasp",
+    "weasel",
+    "whale",
+    "wildcat",
+    "wolf",
+    "wolverine",
+    "wombat",
+    "woodcock",
+    "woodpecker",
+    "worm",
+    "wren",
+    "yak",
+    "zebra"]
+    word = random.choice(words).lower()
+    abc = ('abcdefghijklmnopqrstuvwxyz')
+#list to make sure the player is only using letters when guessing.
+    letters_guessed = []
+#a list to put all the letters in that have already been guessed. This is so the player doesn't use the same letters again.
+    turns = 7
+    guessed = False
+    print()
+    print('The word contains', len(word), 'letters.')
+    print(len(word)*'_')
+    while guessed == False and turns > 0:
+        print('You have ' + str(turns) + ' turns')
+        user = input('Guess a letter in the word or enter the full word: ').lower()
+    #These if statements are the responses to the guesses from the user. Some of it is to tell the user when their answer isn't a letter or if they already guessed the letter before.
+        if len(user) == 1:
+            if user not in abc:
+                print('Not a letter')
+            elif user in letters_guessed:
+                print('This was guessed before')
+            elif user not in word:
+                print('That letter is not part of the word')
+                letters_guessed.append(user)
+                turns-=1
+            elif user in word:
+                print('That letter exist')
+                letters_guessed.append(user)
+            else:
+                print('Invalid answer')
+        elif len(user) == len(word):
+            if user == word:
+                print('You guessed the word correctly. You Win!!!')
+                guessed = True
+                mainmenu()
+            else:
+                print('That is the wrong word')
+                turns -=1
+        else:
+            print('Your guess is not the same length as the word.')
+            turns-=1
+#This is to print the blank spaces when the letters haven't been guessed yet. Goes throght each letter and sees if it is in the list of guessed letters. It also sees when the player guesses the full word.
+        status = ''
+        if guessed == False:
+            for letter in word:
+                if letter in letters_guessed:
+                    status += letter
+                else:
+                    status += '_'
+            print(status)
+        if status == word:
+            print('You guessed the word correctly. You Win!!!')
+            guessed = True
+            mainmenu()
+        elif turns == 0:
+            print('The word was ' +str(word)+ '. You Lose...')
+            mainmenu()
+mainmenu()
